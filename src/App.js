2ba9e9709ef6d4bbc9f2cc7ee13cc363a71203be
Taraansh <taraanshnandwani123@gmail.com>
2023-07-13 20:31:18 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AuthProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <PrivateRoute>
+              <Route path="/Dashboard" element={<Dashboard />} />
+              {/* Add more routes here */}
+            </PrivateRoute>
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
