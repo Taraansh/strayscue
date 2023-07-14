2c4fe62b7254421b5bbde7dd9d755486bc864245
Taraansh <taraanshnandwani123@gmail.com>
2023-07-14 17:21:52 +0530

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-import Header from "./components/Header";
-import PrivateRoute from "./utils/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
 import Dashboard from "./components/Dashboard";
 
@@ -10,13 +8,9 @@ function App() {
     <div className="App">
       <Router>
         <AuthProvider>
-          <Header />
           <Routes>
             <Route path="/" element={<Login />} />
-            <PrivateRoute>
-              <Route path="/Dashboard" element={<Dashboard />} />
-              {/* Add more routes here */}
-            </PrivateRoute>
+            <Route path="/Dashboard" element={<Dashboard />} />
           </Routes>
         </AuthProvider>
       </Router>
