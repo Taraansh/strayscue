0d68523a45c0da350c167723415d29cfec48eb7e
Taraansh <taraanshnandwani123@gmail.com>
2023-07-17 12:14:45 +0530

    Added Home content in herE

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import NavBar from "./NavBar";
-import Home from './Home'
+import { Link } from "react-router-dom";
+import AddModal from "../utils/AddModal";
+import "../styles/Cases.css";
+
 const Dashboard = () => {
   const { user, logoutUser } = useContext(AuthContext);
+  const [modalShow, setModalShow] = React.useState(false);
 
   return user ? (
     <div
@@ -16,7 +20,64 @@ const Dashboard = () => {
       }}
     >
       <NavBar />
-      <Home/>
+      <>
+        <div
+          style={{
+            paddingTop: "3rem",
+            width: "100vw",
+          }}
+        >
+          <hr />
+          <h4 style={{ marginLeft: "10px" }}>Dashboard</h4>
+          <div className="cases">
+            <div className="case-card">
+              <h3 style={{ fontWeight: "bold", marginBottom: "5px" }}>1</h3>
+              <p>Total Cases</p>
+            </div>
+
+            <div className="case-card">
+              <h3 style={{ fontWeight: "bold", marginBottom: "5px" }}>1</h3>
+              <p>Reported</p>
+            </div>
+
+            <div className="case-card">
+              <h3 style={{ fontWeight: "bold", marginBottom: "5px" }}>0</h3>
+              <p>Admitted</p>
+            </div>
+
+            <div className="case-card">
+              <h3 style={{ fontWeight: "bold", marginBottom: "5px" }}>0</h3>
+              <p>Released</p>
+            </div>
+          </div>
+
+          <div className="case-lists mx-auto">
+            <h4 style={{ marginLeft: "1rem" }}>Case Lists</h4>
+            <hr />
+
+            <div className="menu1">
+              <Link
+                onClick={() => setModalShow(true)}
+                style={{
+                  background: "rgb(245, 145, 32)",
+                  color: "#ffffff",
+                  cursor: "pointer",
+                }}
+                className="btn "
+              >
+                <i
+                  style={{ fontSize: "1.3rem" }}
+                  className="fa-light fa-plus"
+                ></i>
+                Add Case
+              </Link>
+              <input type="text" placeholder="Search by location, status etc" />
+            </div>
+          </div>
+        </div>
+
+        <AddModal show={modalShow} onHide={() => setModalShow(false)} />
+      </>
       <i
         className="fa-solid fa-right-from-bracket"
         style={{
