f9ed2ef06e79ad3d3e257543575b9fce1caa29d3
Taraansh <taraanshnandwani123@gmail.com>
2023-07-18 11:58:19 +0530

    Made a POST req to add details

--- src/User Management/CaseMedicalDetails.js
+++ src/User Management/CaseMedicalDetails.js
@@ -1,8 +1,107 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState }from "react";
 import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function CaseMedicalDetails() {
-  const { date } = useContext(AuthContext);
+  const { case_id } = useContext(AuthContext)
+  const [medicalHistory, setMedicalHistory] = useState(null);
+  const [vaccinationStatus, setVaccinationStatus] = useState(null);
+  const [dewormed, setDewormed] = useState(null);
+  const [fitForSurgery, setFitForSurgery] = useState(null);
+  const [otherDetails, setOtherDetails] = useState(null);
+  const [admissionDate, setAdmissionDate] = useState(null);
+  const [feedingRecordImage, setFeedingRecordImage] = useState(null);
+  const [feedingRecordImagePreview, setFeedingRecordImagePreview] = useState("");
+  const [bloodReportImage, setBloodReportImage] = useState(null);
+  const [bloodReportImagePreview, setBloodReportImagePreview] = useState("");
+  const navigate = useNavigate();
+
+  const handleFeedingRecordImage = (event) => {
+    const file = event.target.files[0];
+    setFeedingRecordImage(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setFeedingRecordImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setFeedingRecordImagePreview("");
+    }
+  };
+
+  const handleBloodReportImage = (event) => {
+    const file = event.target.files[0];
+    setBloodReportImage(file);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setBloodReportImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setBloodReportImagePreview("");
+    }
+  };
+
+const handleMedicalDetails = async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append("bloodReportImage", bloodReportImage);
+  formData.append("feedingRecordImage", feedingRecordImage);
+  formData.append("medicalHistory", medicalHistory ? medicalHistory : "");
+  formData.append("vaccinationStatus", vaccinationStatus ? vaccinationStatus : "");
+  formData.append("dewormed", dewormed ? dewormed : "");
+  formData.append("fitForSurgery", fitForSurgery ? fitForSurgery : "");
+  formData.append("otherDetails", otherDetails ? otherDetails : "");
+  formData.append("admissionDate", admissionDate ? admissionDate : "1111-11-11");
+  formData.append("case_linked", case_id);
+
+  console.log(formData.get("medicalHistory"))
+  console.log(formData.get("vaccinationStatus"))
+  console.log(formData.get("dewormed"))
+  console.log(formData.get("fitForSurgery"))
+  console.log(formData.get("otherDetails"))
+  console.log(formData.get("admissionDate"))
+  console.log(formData.get("case_linked"))
+
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/cases/addmedical/",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    if (response.status === 201) {
+      console.log("Success:", response.data);
+      alert("Medical Details Added Successfully");
+      // Handle success or display a success message.
+    } else {
+      console.error("Error:", response.data);
+      // Handle error or display an error message.
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    // Handle error or display an error message.
+  }    
+
+}
+
+  const handleSaveExit = (e) => {
+    handleMedicalDetails(e);
+    navigate("/Dashboard");
+  };
+
+  const handleSaveNext = (e) => {
+    handleMedicalDetails(e);
+  };
 
   return (
     <div className="my-3">
@@ -21,6 +120,7 @@ export default function CaseMedicalDetails() {
               aria-describedby="medicalHistoryHelp"
               name="medicalHistory"
               placeholder="Medical History / Other Issues"
+              onChange={(e)=> setMedicalHistory(e.target.value)}
             />
           </div>
         </div>
@@ -34,8 +134,10 @@ export default function CaseMedicalDetails() {
               className="form-select"
               aria-label="Animal Vaccinated"
               name="vaccinationStatus"
+              onChange={(e)=> setVaccinationStatus(e.target.value)}
+
             >
-              {/* <option value="ChooseBreed">Choose Breed</option>*/}
+              <option value="">Choose Vaccination Status</option>
               <option value="Already Done">Already Done</option>
               <option value="To be done in NGO">To be done in NGO</option>
               <option value="Not Done">Not Done</option>
@@ -53,8 +155,10 @@ export default function CaseMedicalDetails() {
               className="form-select"
               aria-label="Animal Dewormed"
               name="dewormed"
+              onChange={(e)=> setDewormed(e.target.value)}
+
             >
-              {/* <option value="ChooseBreed">Choose Breed</option>*/}
+              <option value="">Choose</option>
               <option value="Already Done">Already Done</option>
               <option value="To be done in NGO">To be done in NGO</option>
               <option value="Not Done">Not Done</option>
@@ -75,8 +179,10 @@ export default function CaseMedicalDetails() {
               className="form-select"
               aria-label="Animal Fit for Surgery"
               name="fitForSurgery"
+              onChange={(e)=> setFitForSurgery(e.target.value)}
+
             >
-              {/* <option value="ChooseBreed">Choose Breed</option>*/}
+              <option value="">Choose</option>
               <option value="Yes">Yes</option>
               <option value="No">No</option>
               <option value="Unsure">Unsure</option>
@@ -95,6 +201,8 @@ export default function CaseMedicalDetails() {
               aria-describedby="otherDetailsHelp"
               name="otherDetails"
               placeholder="Other Details"
+              onChange={(e) => setOtherDetails(e.target.value)}
+
             />
           </div>
         </div>
@@ -107,8 +215,9 @@ export default function CaseMedicalDetails() {
               className="form-control"
               id="admissionDate"
               name="admissionDate"
-              defaultValue={date}
               type="date"
+              onChange={(e) => setAdmissionDate(e.target.value)}
+
             />
           </div>
         </div>
@@ -127,10 +236,18 @@ export default function CaseMedicalDetails() {
                 id="bloodReportImage"
                 accept="image/*"
                 name="bloodReportImage"
-                //   onChange={(event) => setFrontImageFile(event.target.files[0])}
+                onChange={handleBloodReportImage}
               />
             </div>
           </div>
+          {
+            bloodReportImagePreview && (
+              <div>
+                <h6>Preview:</h6>
+                <img src={bloodReportImagePreview} alt="Blood Report Preview" height="100px"/>
+              </div>
+            )
+          }
         </div>
       </div>
 
@@ -147,10 +264,18 @@ export default function CaseMedicalDetails() {
                 id="feedingRecordImage"
                 accept="image/*"
                 name="feedingRecordImage"
-                //   onChange={(event) => setFrontImageFile(event.target.files[0])}
+                onChange={handleFeedingRecordImage}
               />
             </div>
           </div>
+          {
+            feedingRecordImagePreview && (
+              <div>
+                <h6>Preview:</h6>
+                <img src={feedingRecordImagePreview} alt="Feeding Record Preview" height="100px"/>
+              </div>
+            )
+          }
         </div>
       </div>
 
@@ -161,10 +286,10 @@ export default function CaseMedicalDetails() {
         <button type="button" className="btn btn-primary mx-2">
           Exit
         </button>
-        <button type="submit" className="btn btn-primary float-end mx-1">
+        <button type="submit" className="btn btn-primary float-end mx-1" onClick={handleSaveNext}>
           Save & Next
         </button>
-        <button type="submit" className="btn btn-primary float-end mx-1">
+        <button type="submit" className="btn btn-primary float-end mx-1" onClick={handleSaveExit}>
           Save & Exit
         </button>
       </div>
