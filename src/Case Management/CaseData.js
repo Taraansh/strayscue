aac5eb3ca04b396fa7cf12083b12cca70445058e
Taraansh <taraanshnandwani123@gmail.com>
2023-07-20 13:31:30 +0530

    Modified to view all cases by user

--- src/Case Management/CaseData.js
+++ src/Case Management/CaseData.js
@@ -1,6 +1,8 @@
-import { React, useState } from "react";
+import { React, useContext, useState, useEffect } from "react";
+import AuthContext from "../context/AuthContext";
 
 function CaseData() {
+  const { allCases, getAllCases } = useContext(AuthContext);
   const [activeButton, setActiveButton] = useState(0);
 
   const buttonStyle = {
@@ -15,6 +17,10 @@ function CaseData() {
   const handleClick = (index) => {
     setActiveButton(index);
   };
+
+  useEffect(() => {
+    getAllCases();
+  }, [getAllCases]);
 
   return (
     <div className="container-fluid">
@@ -88,24 +94,68 @@ function CaseData() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-          </tr>
+          {allCases.map((data, index) => {
+            // Preprocess the user_profile data to fix the JSON format
+
+            const handleCaseDeleteButton = async (case_id) => {
+              const confirmDelete = window.confirm(
+                "Are you sure you want to delete this order?"
+              );
+              if (confirmDelete) {
+                try {
+                  // Delete the specific order by making an API call
+                  const response = await fetch(
+                    `http://127.0.0.1:8000/cases/delete/${case_id}/`,
+                    {
+                      method: "DELETE",
+                    }
+                  );
+                  if (response.ok) {
+                    // Order successfully deleted, perform any necessary actions (e.g., refresh the order list)
+                    getAllCases(); // Refresh the order list after deletion
+                  } else {
+                    // Handle the case when the delete request fails
+                    console.log("Failed to delete order:", case_id);
+                  }
+                } catch (error) {
+                  // Handle any errors that occur during the delete operation
+                  console.error("Error deleting order:", error);
+                }
+              }
+            };
+
+            return (
+              <tr key={index}>
+                <th scope="row">
+                  {/* {index + 1} */}
+                  <button className="btn btn-primary">Edit Case</button>
+                  <div
+                    className="btn btn-primary mx-1"
+                    onClick={() => {
+                      handleCaseDeleteButton(data.case_id);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                    </svg>
+                  </div>
+                </th>
+                <td>{data.status_of_case}</td>
+                <td>{data.reportingdetail_set[0]?.reporterName}</td>
+                <td>{data.reportingdetail_set[0]?.location}</td>
+                <td>{data.reportingdetail_set[0]?.landmark}</td>
+                <td>{data.reportingdetail_set[0]?.pincode}</td>
+                <td>{data.user_name}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
