65ff042c21dac567dcca8dc8f91c562c6c99310c
Taraansh <taraanshnandwani123@gmail.com>
2023-07-28 12:27:30 +0530

    Modified navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,51 +4,57 @@ import "../styles/NavBar.css";
 import logo from "../assets/logo.png";
 
 function NavBar() {
-  const isSuperUser = localStorage.getItem("is_superuser")
+  const isSuperUser = localStorage.getItem("is_superuser");
+  const stored_type_of_user_in_ngo = localStorage.getItem(
+    "type_of_user_in_ngo"
+  );
 
   return (
     <nav>
       <ul>
         <li className="item">
-            <Link
-              style={{
-                outline: "none",
-                border: "none",
-                color: "#1a202c",
-                fontSize: "14px",
-                boxSizing: "border-box",
-                textDecoration: "none",
-                marginBottom: "1.5rem",
-              }}
-              to="/Dashboard"
-              className="logo"
-            >
-          <div>
+          <Link
+            style={{
+              outline: "none",
+              border: "none",
+              color: "#1a202c",
+              fontSize: "14px",
+              boxSizing: "border-box",
+              textDecoration: "none",
+              marginBottom: "1.5rem",
+            }}
+            to="/Dashboard"
+            className="logo"
+          >
+            <div>
               <img src={logo} alt="" />
-          </div>
-              <span className="nav-items">Strayscue</span>
-            </Link>
+            </div>
+            <span className="nav-items">Strayscue</span>
+          </Link>
         </li>
 
         <li className="item">
-            <Link
-              style={{
-                outline: "none",
-                border: "none",
-                color: "#1a202c",
-                fontSize: "14px",
-                boxSizing: "border-box",
-                textDecoration: "none",
-              }}
-              to="/Dashboard"
-            >
-          <div className="links">
+          <Link
+            style={{
+              outline: "none",
+              border: "none",
+              color: "#1a202c",
+              fontSize: "14px",
+              boxSizing: "border-box",
+              textDecoration: "none",
+            }}
+            to="/Dashboard"
+          >
+            <div className="links">
               <i className="fas fa-home"></i>
               <span className="nav-items">Dashboard</span>
-          </div>
-            </Link>
+            </div>
+          </Link>
         </li>
-        <li className="item">
+        {(isSuperUser === "true" ||
+          stored_type_of_user_in_ngo === "Founder" ||
+          stored_type_of_user_in_ngo === "Manager") && (
+          <li className="item">
             <Link
               style={{
                 outline: "none",
@@ -60,13 +66,15 @@ function NavBar() {
               }}
               to="/UserManagement"
             >
-          <div className="links">
-              <i className="fas fa-solid fa-user"></i>
-              <span className="nav-items">User Management</span>
-          </div>
+              <div className="links">
+                <i className="fas fa-solid fa-user"></i>
+                <span className="nav-items">User Management</span>
+              </div>
             </Link>
-        </li>
-        { (isSuperUser === "true") && <li className="item">
+          </li>
+        )}
+        {isSuperUser === "true" && (
+          <li className="item">
             <Link
               style={{
                 outline: "none",
@@ -78,84 +86,85 @@ function NavBar() {
               }}
               to="/NGO"
             >
-          <div className="links">
-              <i className="fas fa-solid fa-n"></i>
-              <span className="nav-items">NGO Management</span>
-          </div>
+              <div className="links">
+                <i className="fas fa-solid fa-n"></i>
+                <span className="nav-items">NGO Management</span>
+              </div>
             </Link>
-        </li>}
+          </li>
+        )}
         <li className="item">
-            <Link
-              style={{
-                outline: "none",
-                border: "none",
-                color: "#1a202c",
-                fontSize: "14px",
-                boxSizing: "border-box",
-                textDecoration: "none",
-              }}
-              to="/Reporter"
-            >
-          <div className="links">
+          <Link
+            style={{
+              outline: "none",
+              border: "none",
+              color: "#1a202c",
+              fontSize: "14px",
+              boxSizing: "border-box",
+              textDecoration: "none",
+            }}
+            to="/Reporter"
+          >
+            <div className="links">
               <i className="fas fa-solid fa-r"></i>
               <span className="nav-items">Reporter Management</span>
-          </div>
-            </Link>
+            </div>
+          </Link>
         </li>
         <li className="item">
-            <Link
-              style={{
-                outline: "none",
-                border: "none",
-                color: "#1a202c",
-                fontSize: "14px",
-                boxSizing: "border-box",
-                textDecoration: "none",
-              }}
-              to="/Vet"
-            >
-          <div className="links">
+          <Link
+            style={{
+              outline: "none",
+              border: "none",
+              color: "#1a202c",
+              fontSize: "14px",
+              boxSizing: "border-box",
+              textDecoration: "none",
+            }}
+            to="/Vet"
+          >
+            <div className="links">
               <i className="fas fa-solid fa-v"></i>
               <span className="nav-items">Vet Management</span>
-          </div>
-            </Link>
+            </div>
+          </Link>
         </li>
         <li className="item">
-            <Link
-              style={{
-                outline: "none",
-                border: "none",
-                color: "#1a202c",
-                fontSize: "14px",
-                boxSizing: "border-box",
-                textDecoration: "none",
-              }}
-              to="/Sponsor"
-            >
-          <div className="links">
+          <Link
+            style={{
+              outline: "none",
+              border: "none",
+              color: "#1a202c",
+              fontSize: "14px",
+              boxSizing: "border-box",
+              textDecoration: "none",
+            }}
+            to="/Sponsor"
+          >
+            <div className="links">
               <i className="fas fa-solid fa-s"></i>
               <span className="nav-items">Sponser Management</span>
-          </div>
-            </Link>
+            </div>
+          </Link>
         </li>
 
         <li className="item">
-            <Link
-              style={{
-                outline: "none",
-                border: "none",
-                color: "#1a202c",
-                fontSize: "14px",
-                boxSizing: "border-box",
-                textDecoration: "none",
-              }}
-              to="/Settings"
-            >
-          <div className="links">
+          <Link
+            style={{
+              outline: "none",
+              border: "none",
+              color: "#1a202c",
+              fontSize: "14px",
+              boxSizing: "border-box",
+              textDecoration: "none",
+            }}
+            to="/Settings"
+          >
+            <div className="links">
               <i className="fas fa-solid fa-gear"></i>
               <span className="nav-items">Settings</span>
-          </div>
-            </Link>
+            </div>
+          </Link>
         </li>
       </ul>
     </nav>
