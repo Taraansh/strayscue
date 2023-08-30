a2df37f43fd7ce0453d24b8c6f83cb5c64223863
Taraansh <taraanshnandwani123@gmail.com>
2023-08-30 16:45:35 +0530

    Updated delete button style

--- src/Sponsor Management/AddSponsor.js
+++ src/Sponsor Management/AddSponsor.js
@@ -225,9 +225,17 @@ const AddSponsor = () => {
                           alt="Sponsor Logo Preview"
                           height="100px"
                         />
-                        <button onClick={handleDeleteSponsorLogo}>
-                          Delete
-                        </button>
+                        <div className="my-2">
+                           <button onClick={handleDeleteSponsorLogo} className='btn' style={{ background: "#ffffff", border: "1px solid grey", padding: "0.3rem" }}>
+                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16"
+                                        style={{
+                                          background: "transparent", color: "red", // border: "none",
+                                        }}><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                      </svg>
+                         </button>
+                           </div>
+
+
                       </div>
                     )}
                   </div>
