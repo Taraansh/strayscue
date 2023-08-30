d770366a9d7e98336ede9b34e87e18c94be03237
Taraansh <taraanshnandwani123@gmail.com>
2023-08-30 16:46:53 +0530

    Modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <>
-    <hr style={{padding:"0", margin:"0"}}/>
-   <div
-      style={{
-        width: "100%",
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        fontSize: "0.9rem",
-        padding:"0.5rem",
-        textAlign: "center",
-        color: "black",
-        bottom: "0px",
-      }}>
-        Copyright&copy; <span style={{fontWeight:"bold"}}> Strayscue </span> 2023</div>
-   </> 
-  )
+    <div>
+      <hr style={{ padding: "0", margin: "0" }} />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "0.9rem",
+          padding: "0.5rem",
+          textAlign: "center",
+          color: "black",
+          bottom: "0px",
+        }}
+      >
+        Copyright&copy; <span style={{ fontWeight: "bold" }}> Strayscue </span>{" "}
+        2023
+      </div>
+    </div>
+  );
 }
